549f4f9ae50a1d324a4926de089a49c69f58497a
Justin D. <justhewebdev@gmail.com>
2026-06-11 16:52:22 -0400

    added root styles

--- src/stores/useQuizStore.js
+++ src/stores/useQuizStore.js
@@ -1,12 +1,15 @@
-import { ref, computed } from 'vue'
-import { defineStore } from 'pinia'
+import { defineStore } from 'pinia';
 
-export const useCounterStore = defineStore('counter', () => {
-  const count = ref(0)
-  const doubleCount = computed(() => count.value * 2)
-  function increment() {
-    count.value++
+export const useQuizStore = defineStore('quiz', {
+  state: () => {
+    return { score: 0 }
+  },
+  actions: {
+    increment(val = 1) {
+      this.score += val
+    }
+  },
+  getters: {
+
   }
-
-  return { count, doubleCount, increment }
 })
---
+++ src/views/StartView.vue
@@ -0,0 +1,7 @@
+<script></script>
+<template>
+  <h1>Kana Quest</h1>
+  <p>Learn Japanese 1 Flashcard at a time!</p>
+  <button>START!</button>
+</template>
+<style></style>
